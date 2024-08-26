4c12a3371f21dc87952fb9f7adc52519ada8a9e6
annahico <annahico@gmail.com>
2024-08-26 20:15:30 +0200

    feat: trying to solve errors

--- frontend/src/app/main.ts
+++ frontend/src/app/main.ts
@@ -1,11 +1,12 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from '../app/app.module';
+import { AppComponent } from '../app/app.component'; // Asegúrate de que la ruta sea correcta
 import { environment } from '../environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic()
+  .bootstrapComponent(AppComponent) // Usa bootstrapComponent para componentes standalones
   .catch(err => console.error(err));
