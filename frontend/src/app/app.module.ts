dc207a37ad9030a8472c0490bef7b5d9b0b9ad53
annahico <annahico@gmail.com>
2024-08-26 19:24:29 +0200

    fix: fixed almost all mistakes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,21 +2,19 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppHeaderComponent } from '../components/app-header/app-header.component';
-import { AuthorDetailsPageComponent } from '../components/author-details-page/author-details-page.component';
-import { AuthorItemComponent } from '../components/author-item/author-item.component';
-import { AuthorsPageComponent } from '../components/authors-page/authors-page.component';
-import { BookDetailsPageComponent } from '../components/book-details-page/book-details-page.component';
-import { BookItemComponent } from '../components/book-item/book-item.component';
-import { BooksPageComponent } from '../components/books-page/books-page.component';
-import { PopUpAuthorFormComponent } from '../components/pop-up-author-form/pop-up-author-form.component';
-import { PopUpFormComponent } from '../components/pop-up-form/pop-up-form.component';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppHeaderComponent } from './components/app-header/app-header.component';
+import { AuthorDetailsPageComponent } from './components/author-details-page/author-details-page.component';
+import { AuthorItemComponent } from './components/author-item/author-item.component';
+import { AuthorsPageComponent } from './components/authors-page/authors-page.component';
+import { BookDetailsPageComponent } from './components/book-details-page/book-details-page.component';
+import { BookItemComponent } from './components/book-item/book-item.component';
+import { BooksPageComponent } from './components/books-page/books-page.component';
+import { PopUpAuthorFormComponent } from './components/pop-up-author-form/pop-up-author-form.component';
+import { PopUpFormComponent } from './components/pop-up-form/pop-up-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
     AuthorsPageComponent,
     BooksPageComponent,
     BookItemComponent,
@@ -26,7 +24,6 @@ import { AppComponent } from './app.component';
     PopUpFormComponent,
     AppHeaderComponent,
     PopUpAuthorFormComponent,
-    
   ],
   imports: [
     BrowserModule,
@@ -35,6 +32,6 @@ import { AppComponent } from './app.component';
     FormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
 export class AppModule { }
