168e835bac20e741118e0dd755d4c81518ad1293
annahico <annahico@gmail.com>
2024-11-29 10:25:16 +0100

    fix: fixed gh github

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: '/FINAL-PROJECT_Booking-FullStack/' // Cambia <nombre-del-repo> por el nombre de tu repositorio de GitHub
+});
